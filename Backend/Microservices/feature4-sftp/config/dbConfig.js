8e1751d736361f17ee7f65c74a5146da10a16de2
Cydnie Na <111486754+cydniena@users.noreply.github.com>
2025-04-04 20:26:45 +0800

    log

--- Backend/Microservices/feature4-sftp/config/dbConfig.js
+++ Backend/Microservices/feature4-sftp/config/dbConfig.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+console.log(process.env.DB_PASSWORD);  
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
