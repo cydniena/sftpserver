cb5524778adacbe4f7a9edcfac1128b91a26249b
Cydnie Na <111486754+cydniena@users.noreply.github.com>
2025-04-04 20:29:40 +0800

    edit sql config

--- Backend/Microservices/feature4-sftp/src/app.js
+++ Backend/Microservices/feature4-sftp/src/app.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "sftp_transactions_db",
+  database: process.env.DB_NAME || "transactions_db",
 });
 
 db.connect((err) => {
