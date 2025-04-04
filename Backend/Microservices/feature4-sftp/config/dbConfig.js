6a2a8a749d9057c0d2025c9fda8a9bce2f94dc90
Cydnie Na <111486754+cydniena@users.noreply.github.com>
2025-04-04 20:36:43 +0800

    test

--- Backend/Microservices/feature4-sftp/config/dbConfig.js
+++ Backend/Microservices/feature4-sftp/config/dbConfig.js
@@ -8,10 +8,10 @@ console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_NAME:', process.env.DB_NAME);
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'localhost',
+  user: 'root',
+  password: 'YourNewSecureP@ssw0rd!',
+  database: 'transactions_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
